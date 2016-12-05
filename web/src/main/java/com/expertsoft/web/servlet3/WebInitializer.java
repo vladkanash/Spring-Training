2566f4ed802fea5c923c1062090c9947f13346d2
Vlad Kanash <vlad.kanash@gmail.com>
2016-12-05 16:40:18 +0300

    Add basic web app configuration.

---
+++ web/src/main/java/com/expertsoft/web/servlet3/WebInitializer.java
@@ -0,0 +1,24 @@
+package com.expertsoft.web.servlet3;
+
+import com.expertsoft.core.config.DataSourceConfiguration;
+import com.expertsoft.web.config.SpringWebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { SpringWebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { DataSourceConfiguration.class };
+    }
+
+}
