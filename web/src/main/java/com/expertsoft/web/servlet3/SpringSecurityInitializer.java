72fbfe8515d4b7dfb67b3c3bd07f8c6148c1b73e
Vlad Kanash <vlad.kanash@gmail.com>
2016-12-27 16:21:53 +0300

    Spring web config moved to xml.

--- web/src/main/java/com/expertsoft/web/servlet3/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.expertsoft.web.servlet3;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
